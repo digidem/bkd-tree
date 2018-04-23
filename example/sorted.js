43c0475a4133df3724b2a945a078626d6ea37a6d
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-04-23 12:16:21 -1000

    tree construction with branch factor

--- example/sorted.js
+++ example/sorted.js
@@ -1,5 +1,8 @@
 var M = 4+4+4
-var N = 31
+var B = 4
+var N = 12
+var fB = Math.floor(B/2)
+var cB = Math.ceil(B/2)
 
 var rows = []
 for (var i = 0; i < N; i++) {
@@ -14,7 +17,11 @@ function build (rows, depth) {
   var result = []
   ;(function walk (rows, depth, index) {
     if (rows.length === 1) {
-      result[index] = rows[0]
+      if (result[index*B+0] !== undefined) {
+        throw new Error('writing to already-allocated sector ' + index*B)
+      }
+      result[index*B+0] = rows[0]
+      //console.log('set',index*B+0)
     }
     if (rows.length <= 1) return
     var axis = depth % 2
@@ -22,11 +29,27 @@ function build (rows, depth) {
       return a[axis] < b[axis] ? -1 : +1
     })
     var n = Math.floor(rows.length/2)
-    result[index] = rows[n]
-    var left = rows.slice(0,n)
-    var right = rows.slice(n+1)
+    for (var i = 0; i < B; i++) {
+      var j = n+i-fB
+      if (j < 0 || j >= rows.length) {
+        result[index*B+i] = null
+      } else {
+        //console.log('SET',index*B+i)
+        if (result[index*B+i] !== undefined) {
+          throw new Error('writing to already-allocated sector '
+            + (index*B+i))
+        }
+        result[index*B+i] = rows[j]
+      }
+    }
+    var lowN = Math.max(0,n-fB)
+    var hiN = Math.min(rows.length,n+cB)
+    var left = rows.slice(0,lowN)
+    var right = rows.slice(hiN)
     walk(left, depth+1, index*2+1)
     walk(right, depth+1, index*2+2)
   })(rows, 0, 0)
-  console.log(result)
+  for (var i = 0; i < result.length; i++) {
+    console.log(i, result[i])
+  }
 }
