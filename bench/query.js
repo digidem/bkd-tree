6f390923f0b4b88dded3c9299b226780e529bfb4
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-05-11 12:42:07 -1000

    insert benchmark results

---
+++ bench/query.js
@@ -0,0 +1,20 @@
+var raf = require('random-access-file')
+var randombytes = require('crypto').randomBytes
+var tmpdir = require('os').tmpdir()
+var path = require('path')
+
+var dir = process.argv[2]
+
+function storage (name, cb) { cb(null,raf(path.join(dir,name))) }
+
+var bkd = require('../')(storage, {
+  branchFactor: Number(process.argv[3])
+})
+
+var bbox = process.argv.slice(4).map(Number)
+
+var start = Date.now()
+bkd.query(bbox, function (err, rows) {
+  console.log(Date.now()-start)
+  console.log(rows.length)
+})
