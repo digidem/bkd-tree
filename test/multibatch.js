4ad59205888dfb3207c3bf3edb9062e9108072a3
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-05-30 13:23:53 -1000

    failing multibatch test

---
+++ test/multibatch.js
@@ -0,0 +1,48 @@
+var test = require('tape')
+var approxEq = require('approximately-equal')
+var umkd = require('../')
+var ram = require('random-access-memory')
+
+function storage (name, cb) { cb(null, ram()) }
+
+test('multibatch', function (t) {
+  t.plan(5)
+  var N = 5000
+  var bkd = umkd(storage, {
+    branchFactor: 4,
+    type: {
+      point: ['float64be','float64be'],
+      value: ['uint32be']
+    },
+    compare: function (a, b) {
+      return a.value[0] === b.value[0]
+    }
+  })
+  var batches = [
+    [ { type: 'insert', point: [1,2], value: [0] } ],
+    [ { type: 'insert', point: [3,4], value: [1] } ],
+    [
+      { type: 'insert', point: [5,6], value: [2] },
+      { type: 'delete', point: [1,2], value: [0] }
+    ]
+  ]
+  ;(function next (i) {
+    if (i === batches.length) return check()
+    var batch = batches[i]
+    bkd.batch(batch, function (err) {
+      t.error(err)
+      next(i+1)
+    })
+  })(0)
+  function check () {
+    bkd.query([0,0,9,9], function (err, values) {
+      t.error(err)
+      t.deepEqual(values, [
+        { point: [3,4], value: [0] },
+        { point: [5,6], value: [2] }
+      ])
+    })
+  }
+})
+
+function cmp (a, b) { return a.value < b.value ? -1 : +1 }
